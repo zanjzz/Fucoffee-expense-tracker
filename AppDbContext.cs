901409163bc1be0659cb85aaddeeeac03f7f8508
Zanjoe Langa <zdllzd23@gmail.com>
2026-04-30 20:10:22 +0800

    Database fixed

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -13,5 +13,22 @@ namespace Fucoffee.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Income> Incomes { get; set; }
         public DbSet<Expense> Expenses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Tell EF to use RecordedBy as the foreign key for Income -> User
+            modelBuilder.Entity<Income>()
+                .HasOne(i => i.User)
+                .WithMany(u => u.Incomes)
+                .HasForeignKey(i => i.RecordedBy);
+
+            // Tell EF to use RecordedBy as the foreign key for Expense -> User
+            modelBuilder.Entity<Expense>()
+                .HasOne(e => e.User)
+                .WithMany(u => u.Expenses)
+                .HasForeignKey(e => e.RecordedBy);
+        }
     }
 }
